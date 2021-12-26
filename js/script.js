2522d1197b2f5dd58be462bb5798a91385b99e38
Ryan-STT <ryantomaz1090@gmail.com>
2021-12-26 14:55:12 -0300

    a

--- js/script.js
+++ js/script.js
@@ -3,3 +3,48 @@ $(document).ready(function(){
         $("#ExemploModalCentralizado").empty();
     });
 });
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video1").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video2").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video3").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video4").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video5").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video6").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video7").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video8").empty();
+    });
+});
+$(document).ready(function(){
+    $(".close").click(function(){
+        $("#video9").empty();
+    });
+});
