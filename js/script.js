dc2461fdbee7767c090dcafa3ce15129d23c0a37
Ryan-STT <ryantomaz1090@gmail.com>
2021-12-26 15:09:42 -0300

    A

--- js/script.js
+++ js/script.js
@@ -4,47 +4,47 @@ $(document).ready(function(){
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close1").click(function(){
         $("#video1").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close2").click(function(){
         $("#video2").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close3").click(function(){
         $("#video3").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close4").click(function(){
         $("#video4").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close5").click(function(){
         $("#video5").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close6").click(function(){
         $("#video6").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close7").click(function(){
         $("#video7").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close8").click(function(){
         $("#video8").empty();
     });
 });
 $(document).ready(function(){
-    $(".close").click(function(){
+    $(".close9").click(function(){
         $("#video9").empty();
     });
 });
